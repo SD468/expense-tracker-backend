0cf1069e4fec1d1879e323655ef7df3261d621a0
SD468 <105623055+SD468@users.noreply.github.com>
2025-10-19 19:06:58 +0530

    initial commit

---
+++ expanse-tracker-transctional-service/src/main/java/com/expanse/tracker/ExpanseTrackerTransctionalServiceApplication.java
@@ -0,0 +1,13 @@
+package com.expanse.tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpanseTrackerTransctionalServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpanseTrackerTransctionalServiceApplication.class, args);
+	}
+
+}
